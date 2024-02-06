da56ae150e08a34908c92a94161e9e8b14038a93
Rolando <roli.miranda@yahoo.com>
2024-02-06 19:43:18 +0100

    Refactor code and add automated message channel ID and scheduler

--- app.py
+++ app.py
@@ -10,23 +10,23 @@ import time
 import json
 import config
 import os
-
+#--------------------------------------------------------------------------#
 automated_message_channel_id = 1117895284773634128
-
+#--------------------------------------------------------------------------#
 intents = discord.Intents.default()
 intents.message_content = True
 intents.messages = True
 intents.guilds = True
 intents.members = True
-
+#--------------------------------------------------------------------------#
 bot = commands.Bot(command_prefix='!', intents=intents)
-
+#--------------------------------------------------------------------------#
 TOKEN = config.token
-
+#--------------------------------------------------------------------------#
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name} - {bot.user.id}')
-
+#--------------------------------------------------------------------------#
 usage_icons_urls = {
     'very low': 'https://i.imgur.com/J7HFy4H.png',
     'low': 'https://i.imgur.com/6u1Gu9P.png',
@@ -34,7 +34,7 @@ usage_icons_urls = {
     'high': 'https://i.imgur.com/wvpzkrq.png',
     'very high': 'https://i.imgur.com/wvBqMl8.png'
 }
-
+#--------------------------------------------------------------------------#
 def send_automated_message():
     response = requests.get('https://api.nightlight.gg/v1/shrine')
 
@@ -83,17 +83,17 @@ def send_automated_message():
             print("Channel not found.")
     else:
         print("Failed to fetch data from the API.")
-
+#--------------------------------------------------------------------------#
 def run_scheduler():
     while True:
         schedule.run_pending()
         time.sleep(1)
-
-schedule.every().day.at("18:22").do(send_automated_message)
-
+#--------------------------------------------------------------------------#
+schedule.every().day.at("19:20").do(send_automated_message)
+#--------------------------------------------------------------------------#
 scheduler_thread = threading.Thread(target=run_scheduler)
 scheduler_thread.start()
-
+#--------------------------------------------------------------------------#
 @bot.command(name='shrine')
 async def fetch_shrine_info(ctx):
     response = requests.get('https://api.nightlight.gg/v1/shrine')
@@ -139,5 +139,6 @@ async def fetch_shrine_info(ctx):
             await ctx.send(embed=perk_embed)
     else:
         await ctx.send("Failed to fetch data from the API.")
-
+#--------------------------------------------------------------------------#
 bot.run(TOKEN)
+#--------------------------------------------------------------------------#
