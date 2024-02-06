65b232596789c445cb954e5f90f430126b5bbed6
Rolando <roli.miranda@yahoo.com>
2024-02-06 16:57:46 +0100

    Add import statements for config and os

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 import os
 
 token = os.environ['DISCORD_TOKEN']
-=======
-token = 'MTIwNDA0MjAwMDUyNjA5MDI1Mg.GPCOqf.zH60jRLeUqtlmRZwsJqILYZuSyNSJ5RzN5vNM4'
->>>>>>> 891a1391eb51b63a82e63c93efc618d6abafc782
