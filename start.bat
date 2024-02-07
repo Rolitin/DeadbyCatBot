a66db5b71428e8b56cdc8621a9e50ed9b7293763
rolando.puerta <rolando.puerta@testifi.io>
2024-02-07 17:08:45 +0100

    Update Jenkinsfile to fix path for DBD App PID file

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-cd C:\ProgramData\Jenkins\.jenkins\workspace\Dead-by-Cat-Bot
+cd C:\Users\rolando\Documents\Jenkins\.jenkins\workspace\Dead-by-Cat-Bot
 start "DBD App" cmd /K "python app.py"
